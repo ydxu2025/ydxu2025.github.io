24636ce08a306a7e42389b6db5c5bc5455ed2f94
ydxu2025 <steven_xu2000@hotmail.com>
2025-09-20 09:28:27 +0000

    Deploying to gh-pages from @ ydxu2025/ydxu2025.github.io@a8bd542f80471bf2f75168b74207754a925b2afc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
+        },{id: "nav-research",
+          title: "Research",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
