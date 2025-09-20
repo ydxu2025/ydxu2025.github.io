ecc46d87fc847e6ff94b6c1e7f229c3fba3d83d1
ydxu2025 <steven_xu2000@hotmail.com>
2025-09-20 15:13:43 +0000

    Deploying to gh-pages from @ ydxu2025/ydxu2025.github.io@dbf770c5b0e7e6590c5a053a667540442a7254ed 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -38,8 +38,8 @@ ninja.data = [{
             window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          title: "Teaching",
+          description: "Let us study together.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
