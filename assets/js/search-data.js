bea0bb78ce4bea039eba979fedb0f2c704ccb68f
ydxu2025 <steven_xu2000@hotmail.com>
2025-09-21 13:09:37 +0000

    Deploying to gh-pages from @ ydxu2025/ydxu2025.github.io@a0b9169ee5a0f34f4654ccddc7ae65d4056b2fb7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -78,15 +78,15 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-dr-yuedong-xu-has-migrated-to-college-of-computer-science-and-artificial-intelligence-计算与智能创新学院-fudan-university",
           title: 'Dr. Yuedong Xu has migrated to College of Computer Science and Artificial Intelligence...',
           description: "",
-          section: "News",},{id: "news-our-work-on-federated-llm-finetuning-is-accepted-to-neurips-2025-sparkles-smile",
+          section: "News",},{id: "news-dr-yuedong-xu-serves-as-a-phd-advisor-in-shenzhen-loop-area-institute-深圳河套学院",
+          title: 'Dr. Yuedong Xu serves as a PhD advisor in Shenzhen Loop Area Institute...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2/";
+            },},{id: "news-our-work-on-federated-llm-finetuning-is-accepted-to-neurips-2025-sparkles-smile",
           title: 'Our work on Federated LLM Finetuning is accepted to NeurIPS 2025 :sparkles: :smile:...',
           description: "",
           section: "News",},{id: "projects-project-1",
