9c3975f0460b9f61ac20d3d7612679b9c4223272
ydxu2025 <steven_xu2000@hotmail.com>
2025-09-22 00:20:50 +0000

    Deploying to gh-pages from @ ydxu2025/ydxu2025.github.io@24fb054ffbc82bc2921ab00d3c67ff229a05f640 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -94,9 +94,9 @@ ninja.data = [{
           description: "making training faster",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-foundation-model-inference",
+          title: 'Foundation Model Inference',
+          description: "accelerating model inference",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
