deae09118dbaf36b6e71156dfd36cdb8fa343bfb
ydxu2025 <steven_xu2000@hotmail.com>
2025-09-22 00:24:07 +0000

    Deploying to gh-pages from @ ydxu2025/ydxu2025.github.io@e9698ce1a42464d50921bb46f6292b09282eae25 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -99,14 +99,14 @@ ninja.data = [{
           description: "accelerating model inference",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-machine-learning-for-networking",
+          title: 'Machine Learning for Networking',
+          description: "propelling intelligent network resource allocation",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-video-streaming",
+          title: 'Video Streaming',
+          description: "enjoying video streaming with higher quality and less interruptions",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
