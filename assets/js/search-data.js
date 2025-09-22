6aa13922dc9136cca71e62bc1d2df7f83bfdf2c2
ydxu2025 <steven_xu2000@hotmail.com>
2025-09-22 00:40:33 +0000

    Deploying to gh-pages from @ ydxu2025/ydxu2025.github.io@f4a02b80a4518e5104ce26b38c502fa4b31103f4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-people",
-          title: "People",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
         },{id: "dropdown-group-meetings",
               title: "Group Meetings",
               description: "",
