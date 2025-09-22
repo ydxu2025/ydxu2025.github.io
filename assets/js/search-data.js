a9c72832f623f8e784294725f1a4cadcad4ab658
ydxu2025 <steven_xu2000@hotmail.com>
2025-09-22 00:26:01 +0000

    Deploying to gh-pages from @ ydxu2025/ydxu2025.github.io@41c8f731f0ba7a99bcc5ec22959fdc7628e1ff70 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -109,9 +109,9 @@ ninja.data = [{
           description: "enjoying video streaming with higher quality and less interruptions",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-network-economics",
+          title: 'Network Economics',
+          description: "earning more or paying less",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
