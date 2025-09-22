2433c60f42bacec32cd75e0b16cdca69cec48b93
ydxu2025 <steven_xu2000@hotmail.com>
2025-09-22 00:27:56 +0000

    Deploying to gh-pages from @ ydxu2025/ydxu2025.github.io@22bf5a9039c8bd2a4dacc0d72212664b40f1b374 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -104,24 +104,24 @@ ninja.data = [{
           description: "propelling intelligent network resource allocation",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-video-streaming",
+          title: 'Video Streaming',
+          description: "enjoying video streaming with higher quality and less interruptions",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-network-economics",
+          title: 'Network Economics',
+          description: "earning more or paying less",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
+            },},{id: "projects-stochastic-network-modeling",
+          title: 'Stochastic Network Modeling',
+          description: "seeing through certainty from randomness",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
+            },},{id: "projects-wifi-localization-and-sensing",
+          title: 'WiFi Localization and Sensing',
+          description: "fishes, fishes where are you",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
             },},{id: "projects-project-8",
